97119b55051e6bc61641a3644cc80f630b257bcc
Codjam <jamaltbutler>
2018-03-04 15:48:27 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Goku",
+    "photo" : "http://www.mens-hairstylists.com/wp-content/uploads/2015/10/stylish-haircuts-for-men.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Vegita",
+    "photo" : "http://science-all.com/images/wallpapers/girl-pictures/girl-pictures-23.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Krillin",
+    "photo" : "https://scontent-sea1-1.cdninstagram.com/vp/d21ba497036e69629b00fa464e144645/5B1930EA/t51.2885-15/s480x480/e15/11380138_1623310971247304_1199768349_n.jpg?ig_cache_key=OTk0NjA3ODUwNTg3MzMxMTM1.2",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Bulma",
+    "photo" : "https://media.giphy.com/media/3o7TKTACWtKTFptiec/source.gif",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Piccolo",
+    "photo" : "https://78.media.tumblr.com/5a72fde9c1bd9fe784eef4ed3b0f24d4/tumblr_p1dq09de5K1ujn35ko1_1280.gif",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Android18",
+    "photo" : "https://pbs.twimg.com/media/Cy96ikZWEAABLgU.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Gohan",
+    "photo" : "https://otakukart.com/wp-content/uploads/2017/10/Gohan6.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Chi_Chi",
+    "photo" : "https://vignette.wikia.nocookie.net/dbz-dokkanbattle/images/6/6b/Card_1000500_bg.jpg/revision/latest?cb=20150920104531",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Trunks",
+    "photo" : "https://vignette.wikia.nocookie.net/dragonballfanon/images/0/01/Future_trunks_alt_costume_by_Rockeruz.jpg/revision/latest?cb=20120908162435",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Frieza",
+    "photo" : "https://orig00.deviantart.net/594c/f/2011/027/2/d/nightmare_fuel_by_chaoscroc-d387ipj.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Majin_Boo",
+    "photo" : "http://d.facdn.net/art/canime/1516597684/1516597684.canime_majin_android_21_smol.png",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Shenron",
+    "photo" : "https://wallpapertag.com/wallpaper/full/4/3/6/789864-download-free-wallpaper-hd-shenron-1920x1080-retina.jpg",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Launch",
+    "photo" : "https://vignette.wikia.nocookie.net/dragonball/images/3/35/Launch.Ep.092.png/revision/latest?cb=20120424220711",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Videl",
+    "photo" : "https://i.pinimg.com/736x/db/1f/d3/db1fd3877c7dadeaaaee63cba8f1e22a.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
